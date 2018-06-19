bd1184ab35890dcbd2e57d868c7ecfd6dbc1e80b
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-19 15:36:16 -0700

    Added option to replay game

--- game.py
+++ game.py
@@ -8,34 +8,56 @@
 # if correct, congratulate
 
 import random
-
 user_name = input("Hi, what's your name?\n>")
-print("{}, I'm thinking of a number between 1 and 100. Try to guess my number.".format(user_name))
 
 
-random_number = random.randint(0, 100)
+def game_play():
 
-print(random_number)
+    print(f"{user_name}, I'm thinking of a number between 1 and 100. Try to guess my number.")
 
-counter = 1
+    random_number = random.randint(0, 100)
 
-while True:
+    print(random_number)
+
+    counter = 1
+    best_score = 0
+
     while True:
-        try:
-            user_guess = int(input("Your guess?\n>"))
+        while True:
+            try:
+                user_guess = int(input("Your guess?\n>"))
+                break
+            except ValueError:
+                print("That wasn't an integer! Try again.")
+
+        if user_guess == random_number:
             break
-        except ValueError:
-            print("That wasn't an integer! Try again.")
 
-    if user_guess == random_number:
-        break
+            # print(f"Congratulations {user_name} You guessed the number in {counter} tries!")
+            # new_game = input("Do you want to play again (Y/N)?\n>")
+            # if new_game == "N":
+            #     break
+            # elif new_game == "Y":
+            #     game_play()
+            # else:
+            #     print("That is not a valid answer. Good bye.")
+            #     break
+        if user_guess not in range(1, 101):
+            print("That is not a valid answer, please guess again.")
+        elif user_guess > random_number:
+            print("Your guess is too high. Try again.")
+        else:
+            print("Your guess is too low. Try again.")
+        counter += 1
 
-    if user_guess not in range(1, 101):
-        print("That is not a valid answer, please guess again.")
-    elif user_guess > random_number:
-        print("Your guess is too high. Try again.")
+    print(f"Congratulations {user_name} You guessed the number in {counter} tries!")
+    new_game = input("Do you want to play again (Y/N)?\n>")
+    if new_game == "N":
+        print("Ok, bye.")
+    elif new_game == "Y":
+        game_play()
     else:
-        print("Your guess is too low. Try again.")
-    counter += 1
+        print("That is not a valid answer. Good bye.")
 
-print(f"Congratulations {user_name} You guessed the number in {counter} tries!")
+
+game_play()

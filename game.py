faaa05ea548237f846a6c38c8008cd512ace0dee
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-19 16:02:51 -0700

    Track Best Score

--- game.py
+++ game.py
@@ -9,9 +9,9 @@
 
 import random
 user_name = input("Hi, what's your name?\n>")
+best_score = 10000000
 
-
-def game_play():
+def game_play(score):
 
     print(f"{user_name}, I'm thinking of a number between 1 and 100. Try to guess my number.")
 
@@ -20,7 +20,6 @@ def game_play():
     print(random_number)
 
     counter = 1
-    best_score = 0
 
     while True:
         while True:
@@ -32,16 +31,6 @@ def game_play():
 
         if user_guess == random_number:
             break
-
-            # print(f"Congratulations {user_name} You guessed the number in {counter} tries!")
-            # new_game = input("Do you want to play again (Y/N)?\n>")
-            # if new_game == "N":
-            #     break
-            # elif new_game == "Y":
-            #     game_play()
-            # else:
-            #     print("That is not a valid answer. Good bye.")
-            #     break
         if user_guess not in range(1, 101):
             print("That is not a valid answer, please guess again.")
         elif user_guess > random_number:
@@ -50,14 +39,18 @@ def game_play():
             print("Your guess is too low. Try again.")
         counter += 1
 
-    print(f"Congratulations {user_name} You guessed the number in {counter} tries!")
+    print(f"Congratulations {user_name} ! You guessed the number in {counter} tries!")
+    if counter <= score:
+        score = counter
+        print("Congrats! That's a new best score!")
+        
     new_game = input("Do you want to play again (Y/N)?\n>")
     if new_game == "N":
         print("Ok, bye.")
     elif new_game == "Y":
-        game_play()
+        game_play(score)
     else:
         print("That is not a valid answer. Good bye.")
 
 
-game_play()
+game_play(best_score)

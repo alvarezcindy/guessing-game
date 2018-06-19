c52270b653718c437ce28b3e9423376a78637236
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-19 16:51:06 -0700

    Score Conversions

--- game.py
+++ game.py
@@ -9,13 +9,19 @@
 
 import random
 user_name = input("Hi, what's your name?\n>")
-best_score = 4
+best_score = 100
+
 
 def game_play(score):
 
-    print(f"{user_name}, I'm thinking of a number between 1 and 100. Try to guess my number.")
+    print("The score is", score)
+    print("You get to set your own range. Pick a starting number and an ending number.")
+    start_num = int(input("Start number >"))
+    end_num = int(input("End number >"))
 
-    random_number = random.randint(1, 100)
+    print(f"{user_name}, I'm thinking of a number between and including {start_num} and {end_num}. Try to guess my number.")
+
+    random_number = random.randint(start_num, end_num)
 
     print(random_number)
 
@@ -29,11 +35,11 @@ def game_play(score):
             except ValueError:
                 print("That wasn't an integer! Try again.")
 
-        if counter > 5:
-            print("That's all the guesses you get.")
-            break
         if user_guess == random_number:
             print(f"Congratulations {user_name} ! You guessed the number in {counter} tries!")
+            break
+        if counter > 10:
+            print("That's all the guesses you get.")
             break
         if user_guess not in range(1, 101):
             print("That is not a valid answer, please guess again.")
@@ -43,11 +49,15 @@ def game_play(score):
             print("Your guess is too low. Try again.")
         counter += 1
 
-    
+    if len(range(start_num, end_num)) <= 50:
+        counter = int(counter*2)
+    elif len(range(start_num, end_num)) > 100:
+        counter = int(counter/2)
+
     if counter <= score:
         score = counter
         print("Congrats! That's a new best score!")
-        
+
     new_game = input("Do you want to play again (Y/N)?\n>")
     if new_game.upper() == "N":
         print("Ok, bye.")

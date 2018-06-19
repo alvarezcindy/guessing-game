49a18f76471f046b17ef0e7235877038cf7e10c8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-06-19 16:09:00 -0700

    Added limit on number of guesses

--- game.py
+++ game.py
@@ -9,13 +9,13 @@
 
 import random
 user_name = input("Hi, what's your name?\n>")
-best_score = 10000000
+best_score = 4
 
 def game_play(score):
 
     print(f"{user_name}, I'm thinking of a number between 1 and 100. Try to guess my number.")
 
-    random_number = random.randint(0, 100)
+    random_number = random.randint(1, 100)
 
     print(random_number)
 
@@ -29,7 +29,11 @@ def game_play(score):
             except ValueError:
                 print("That wasn't an integer! Try again.")
 
+        if counter > 5:
+            print("That's all the guesses you get.")
+            break
         if user_guess == random_number:
+            print(f"Congratulations {user_name} ! You guessed the number in {counter} tries!")
             break
         if user_guess not in range(1, 101):
             print("That is not a valid answer, please guess again.")
@@ -39,15 +43,15 @@ def game_play(score):
             print("Your guess is too low. Try again.")
         counter += 1
 
-    print(f"Congratulations {user_name} ! You guessed the number in {counter} tries!")
+    
     if counter <= score:
         score = counter
         print("Congrats! That's a new best score!")
         
     new_game = input("Do you want to play again (Y/N)?\n>")
-    if new_game == "N":
+    if new_game.upper() == "N":
         print("Ok, bye.")
-    elif new_game == "Y":
+    elif new_game.upper() == "Y":
         game_play(score)
     else:
         print("That is not a valid answer. Good bye.")
